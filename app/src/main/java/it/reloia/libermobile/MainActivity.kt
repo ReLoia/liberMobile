c6e5ba6e234238ff5d61d16ab6d6391f4fadccaa
ReLoia <zrennyz@gmail.com>
2024-06-30 22:12:33 +0200

    First Commit Setup the MainActivity for the project

---
+++ app/src/main/java/it/reloia/libermobile/MainActivity.kt
@@ -0,0 +1,42 @@
+package it.reloia.libermobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import it.reloia.libermobile.ui.theme.LiberMobileTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LiberMobileTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize()
+                    ) { innerPadding ->
+                        Surface(
+                            modifier =
+                                Modifier
+                                    .fillMaxSize()
+                                    .padding(innerPadding)
+                        ) {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
